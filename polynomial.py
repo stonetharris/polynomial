ce741c46379d1bf1b2ce3d2fff998ecfeb1794d9
Stone Harris <stonetharris@gmail.com>
2024-01-28 11:08:36 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, value):
+        return value
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,6 +42,9 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
 
 class Div:
     def __init__(self, p1, p2):
@@ -51,6 +63,11 @@ class Div:
             p2_repr = repr(self.p2)
             
         return p1_repr + " / " + p2_repr
+    
+    def evaluate(self, value):
+        if self.p2.evaluate(value) == 0:
+            raise ZeroDivisionError("division by zero")
+        return self.p1.evaluate(value) / self.p2.evaluate(value)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -58,10 +75,16 @@ class Sub:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " - ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " - " + repr(self.p2)
+        p1_repr = repr(self.p1)
+        if isinstance(self.p2, (Add, Sub, Div)):
+            p2_repr = "( " + repr(self.p2) + " )"
+        else:
+            p2_repr = repr(self.p2)
+        return p1_repr + " - " + p2_repr
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) - self.p2.evaluate(value)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
